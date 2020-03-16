9752a33f1f580b531bdc19acf82471a5c8ea7f41
Jeroen Thora <jeroenthora@gmail.com>
2020-03-16 10:52:13 +0100

    [AllBundles] Add configuration option to disable permission module/checking

---
+++ src/Kunstmaan/AdminBundle/DependencyInjection/Compiler/EnablePermissionsPass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Kunstmaan\AdminBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class EnablePermissionsPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasParameter('kunstmaan_node.permissions.enabled')) {
+            $permissionsEnabledParameter = $container->getParameter('kunstmaan_node.permissions.enabled');
+
+            $container->getDefinition('kunstmaan_admin.acl.helper')->setArgument('$permissionsEnabled', $permissionsEnabledParameter);
+            $container->getDefinition('kunstmaan_admin.acl.native.helper')->setArgument('$permissionsEnabled', $permissionsEnabledParameter);
+            $container->getDefinition('kunstmaan_admin.security.acl.voter')->setArgument('$permissionsEnabled', $permissionsEnabledParameter);
+        }
+    }
+}
